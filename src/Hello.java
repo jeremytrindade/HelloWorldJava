de2603cc266bdd7475933b9d2c5034be56f46573
Jérémy DA TRINDADE <jeremytrindade@gmail.com>
2018-04-18 00:20:57 +0200

    Et maintenant plus quelques variables et calcules

--- src/Hello.java
+++ src/Hello.java
@@ -1,5 +1,17 @@
 public class Hello {
     public static void main(String[] args){
+
         System.out.println("Hello, Je m'appelle Jérémy!");
+
+        int myFirstNumber = (10 + 5) + (2 * 10);
+        int mySecundNumber = 12;
+        int myThirdnumber = myFirstNumber * 2;
+
+        int myTotal = myFirstNumber + mySecundNumber + myThirdnumber;
+
+        int myLastOne = 1000 - myTotal;
+
+        System.out.println(myLastOne);
+        System.out.println("myLastOne");
     }
 }
